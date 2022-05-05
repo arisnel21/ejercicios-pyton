c8d8f0b5077971645a1411d7cf4c7815105f8aaf
Alumnos Santiago Centro <alumnos09@inacap.cl>
2022-05-05 19:26:19 -0400

    if como switch

--- ejem_if.py
+++ ejem_if.py
@@ -1,9 +1,16 @@
-num = int(input("ingrese primer numero: "))
-numDos = int(input("ingrese segundo numero "))
+opcion = int(input("Ingrese la opcion a realizar por favor: "))
 
-if(num>numDos): 
-    print("el primer numero es mayor")
-elif(numDos>num):
-    print("el segundo numero es mayor")
-else:
-    print("los numeros son iguales")
+if(opcion == 1):
+    print("BIENVENIDOS A LA SUMA!!!")
+    a = int(input("ingrese Primer Numero: "))
+    b = int(input("ingrese segundo Numero: "))
+    b = a+b
+    print("El resultado de la suma es: ",b)
+elif(opcion ==2):
+    print("BIENVENIDOS A LA RESTA!!!")
+    a = int(input("ingrese Primer Numero: "))
+    b = int(input("ingrese segundo Numero: "))
+    b = a-b
+    print("El resultado de la suma es: ",b)
+else: 
+    print("OPCION INGRESADA INVALIDA!!!!")
